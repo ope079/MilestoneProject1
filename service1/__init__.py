3a500d30aec9440cdf73d8b010f51b3277486fc7
ope079 <o_orekoya@yahoo.com>
2021-01-29 12:10:45 +0000

    commit

--- service1/__init__.py
+++ service1/__init__.py
@@ -1,6 +1,37 @@
 import logging
 import requests
+import cosmos_get_started
 import azure.functions as func
+from azure.cosmos import exceptions, CosmosClient, PartitionKey
+
+
+    
+# Initialize the Cosmos client
+endpoint = 'https://milestone.documents.azure.com:443/'
+key = 'K3ceqtLPD6EhUNhKo5CiGlIiDOzZPaqV37lFqps7408O8kIqddhXavClXctzM528oRTfPE0mZf2ZndTGiumbnQ=='
+
+# <create_cosmos_client>
+client = CosmosClient(endpoint, key)
+# </create_cosmos_client>
+
+# Create a database
+# <create_database_if_not_exists>
+database_name = 'RandomDatabase'
+database = client.create_database_if_not_exists(id=database_name)
+# </create_database_if_not_exists>
+
+# Create a container
+# Using a good partition key improves the performance of database operations.
+# <create_container_if_not_exists>
+container_name = 'RandomDBContainer'
+container = database.create_container_if_not_exists(
+    id=container_name,
+    partition_key=PartitionKey(path="/random"), 
+    offer_throughput=400
+)
+# </create_container_if_not_exists>
+
+
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -9,43 +40,16 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     rand_val =  rand_int.text + rand_str.text
 
 
-    # Initialize the Cosmos client
-    endpoint = "https://milestone.documents.azure.com:443/"
-    key = 'K3ceqtLPD6EhUNhKo5CiGlIiDOzZPaqV37lFqps7408O8kIqddhXavClXctzM528oRTfPE0mZf2ZndTGiumbnQ=='
-
-    # <create_cosmos_client>
-    client = CosmosClient(endpoint, key)
-    # </create_cosmos_client>
-
-    # Create a database
-    # <create_database_if_not_exists>
-    database_name = 'AzureRandomDatabase'
-    database = client.create_database_if_not_exists(id=database_name)
-    # </create_database_if_not_exists>
-
-    # Create a container
-    # Using a good partition key improves the performance of database operations.
-    # <create_container_if_not_exists>
-    container_name = 'RandomDBContainer'
-    container = database.create_container_if_not_exists(
-        id=container_name, 
-        partition_key=PartitionKey(path="/lastName"),
-        offer_throughput=400
-    )
-    # </create_container_if_not_exists>
-
-
     # Add items to the container
     random_items_to_create = [rand_val]
 
     # <create_item>
-    container.create_item(body=random_items_to_creat)
+    for random_item in random_items_to_create:
+        cosmos_get_started.container.create_item(body=random_item)
     # </create_item>
 
-
-
-
-
+    
+    
     return func.HttpResponse(
         str(rand_val),
         status_code=200
--- service3/__init__.py
+++ service3/__init__.py
@@ -4,9 +4,7 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    rand_str = ''.join(random.choice(string.ascii_lowercase) for i in range(5))
+    rand_str = ''.join(random.choice("abcdefghijklmnopqrstuvwxyz") for i in range(5))
     return func.HttpResponse(
         str(rand_str),
         status_code=200
