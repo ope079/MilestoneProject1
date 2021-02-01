840fa0566123b0c26abb25e6e358a98eacb5da4a
ope079 <o_orekoya@yahoo.com>
2021-02-01 17:19:35 +0000

    database layer

--- Service2/__init__.py
+++ Service2/__init__.py
@@ -1,4 +1,4 @@
-import logging
+
 import random
 import azure.functions as func
 
--- service1/__init__.py
+++ service1/__init__.py
@@ -1,56 +1,42 @@
-import logging
 import requests
-import cosmos_get_started
 import azure.functions as func
 from azure.cosmos import exceptions, CosmosClient, PartitionKey
 
 
-    
-# Initialize the Cosmos client
-endpoint = 'https://milestone.documents.azure.com:443/'
-key = 'K3ceqtLPD6EhUNhKo5CiGlIiDOzZPaqV37lFqps7408O8kIqddhXavClXctzM528oRTfPE0mZf2ZndTGiumbnQ=='
 
-# <create_cosmos_client>
+endpoint = ''
+key = ''
+
+
 client = CosmosClient(endpoint, key)
-# </create_cosmos_client>
 
-# Create a database
-# <create_database_if_not_exists>
 database_name = 'RandomDatabase'
 database = client.create_database_if_not_exists(id=database_name)
-# </create_database_if_not_exists>
 
-# Create a container
-# Using a good partition key improves the performance of database operations.
-# <create_container_if_not_exists>
 container_name = 'RandomDBContainer'
 container = database.create_container_if_not_exists(
     id=container_name,
     partition_key=PartitionKey(path="/random"), 
     offer_throughput=400
 )
-# </create_container_if_not_exists>
+
 
 
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    rand_int = requests.get('https://milestonep.azurewebsites.net/api/Service2?code=rwdH4XLcYAnu6OJhqfTamF/NKcXEVODa4L15Ok1XuYwwexVl8LXkWA==')
-    rand_str = requests.get('https://milestonep.azurewebsites.net/api/service3?code=/c0gAOFQYqflLf3ePnKzPTuc4yMAtSMjvii05pzGLqjI746D9KwXzw==')
-    rand_val =  rand_int.text + rand_str.text
-
-
-    # Add items to the container
-    random_items_to_create = [rand_val]
-
-    # <create_item>
-    for random_item in random_items_to_create:
-        cosmos_get_started.container.create_item(body=random_item)
-    # </create_item>
-
+    rand_int = requests.get('https://function079.azurewebsites.net/api/Service2?code=OQ7NqJoVXySydhZ8iFV/9O849dseyfcr1SIC9f7GFok0iFMCaDS0zQ==')
+    rand_str = requests.get('https://function079.azurewebsites.net/api/service3?code=T/WnvvaZuwb8i68eBItAgI6rVruEyafQvagmSmeaCzp836G1UiwZ7A==')
+    rand_val = rand_int.text + rand_str.text
     
+
+    container.create_item(body={
+        "id": 1, 
+        "random_name" : rand_val
+    })
+   
     
     return func.HttpResponse(
-        str(rand_val),
+        rand_val,
         status_code=200
     )
